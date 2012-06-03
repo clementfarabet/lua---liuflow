c3dc0dde2063b8fecd40fb64bde29d0889a87518
Clement Farabet <clement.farabet@gmail.com>
2012-06-03 18:00:44 -0400

    Removed build dir

--- build/CMakeFiles/documentation-dok.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/documentation-dok"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/documentation-dok.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
